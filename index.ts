b11cc97025dc7da180e552a874c7d780b95cd0bb
JOHN LENNON DE MELO FERNANDES <7700163902@bitbucket.viavarejo.com.br>
2018-12-14 23:18:01 -0200

    adicionando textos ao index.js

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import { HolesCounter } from './src/holes-counter/holes-counter'
 
-
+const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 const holesCounter = new HolesCounter();
-const result = holesCounter.setText('b').getQuantityOfHoles();
+const result = holesCounter.setText(text).getQuantityOfHoles();
 
-console.log(result);
+console.log(`O texto Abaixo tem : ${result} buracos`);
+console.log(text);
