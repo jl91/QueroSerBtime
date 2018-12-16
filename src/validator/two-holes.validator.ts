2b2c0a112422ae24c37d3607a97b76ff0e8dc955
JOHN LENNON DE MELO FERNANDES <7700163902@bitbucket.viavarejo.com.br>
2018-12-16 21:14:37 -0200

    adicionando regratoração de texto

--- src/validator/two-holes.validator.ts
+++ src/validator/two-holes.validator.ts
@@ -6,6 +6,10 @@ import { Injectable } from "ts-di";
 @Injectable()
 export class TwoHolesValidator implements ValidatorInterface<string> {
 
+    // Parece meio estranho criar um array para apenas uma letra mas no caso do enunciado tive alguns problemas.
+    // primeiro o enunciado define a regra de Holes somente para letras e não números.
+    // Pelo que eu pude entender, seguindo essa regra  apenas a letra "b" maiúscula contém 2 buracos,
+    // claro que o 8 poderia conter também, porém não é letra.
     private readonly TWO_HOLES_LETTERS: Array<string> = [
         'B'
     ];
